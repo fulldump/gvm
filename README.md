d1f03a901fa34e33ebfed3ee7dfa6ef8bbef6b60
fulldump <gerardooscarjt@gmail.com>
2017-03-31 17:42:02 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# gvm
-Golang Version Manager
+# Golang Version Manager
+
+Here is a small script to manage several golang versions in Ubuntu.
+
+## How to install
+
+```sh
+
+```
+
+## How to use
+
+```sh
+gvm set 1.7.5
+```
+
+That will download and configure your environment for golang `1.7.5`.
+
+If you want to see all installed versions:
+
+```sh
+gvm list
+```
+
---
+++ gvm.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+SCRIPT_FILENAME=`readlink -f $0`
+SCRIPT_DIR=`dirname $SCRIPT_FILENAME`
+
+COMMAND=$1
+
+if [ "version" == $COMMAND ] ; then
+	go version
+
+	exit
+fi
+
+if [ "list" == $COMMAND ] ; then
+	cd "$SCRIPT_DIR"
+
+	for i in `ls -d */`; do echo ${i%%/}; done
+
+	exit
+fi
+
+if [ "set" == $COMMAND ] ; then
+	cd "$SCRIPT_DIR"
+
+	VERSION=$2
+	FILENAME="go${VERSION}.linux-amd64.tar.gz"
+
+	# Check if folder exists
+	DIR="$SCRIPT_DIR/$VERSION"
+	if [ ! -d "$DIR/go" ] ; then
+		echo "El directorio no existe en local, me lo descargo..."
+
+		# Create dir
+		mkdir -p "$DIR"
+		cd "$DIR"
+
+		# Download
+		wget -c "https://storage.googleapis.com/golang/${FILENAME}"
+
+		# Unzip
+		tar -xzf "$FILENAME"
+	fi
+
+	# Set links
+	ln -fs "$SCRIPT_DIR/$2/go/bin/go" "$SCRIPT_DIR/go"
+	ln -fs "$SCRIPT_DIR/$2/go" "$SCRIPT_DIR/.goroot"
+
+	exit
+fi
+
+if [ "install" == $COMMAND ] ; then
+
+	sudo ln -fs "$SCRIPT_DIR/gvm.sh" /usr/bin/gvm
+
+	sudo ln -fs "$SCRIPT_DIR/go" /usr/bin/go	
+	sudo ln -fs "$SCRIPT_DIR/.goroot" /usr/local/go
+
+	exit
+fi
+
+echo "Invalid command '$COMMAND'"
+
+help=$(cat <<'ZZZZZZHEREDOC'
+version	Get current version
+list	See all local available versions
+set	Set go version
+install	Install this script
+ZZZZZZHEREDOC
+)
+
+echo "$help"
